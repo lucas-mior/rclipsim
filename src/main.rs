ef5dda4066e5fad18cc2f89e143e47a0c991a059
lucas-mior <lucas.mior@tutamail.com>
2023-03-24 14:43:18 -0300

    fix threads

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,49 @@
 use std::env;
+use std::process;
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
 
 fn client_speak_fifo(command: &str, id: Option<&str>) {
     match id {
         Some(id) => println!("Command received: {} with id {}", command, id),
         None => println!("Command received: {}", command),
+    }
+}
+
+fn fifo_thread(lock: Arc<Mutex<()>>) {
+    loop {
+        let guard = lock.lock().unwrap();
+        println!("fifo_thread");
+        drop(guard);
+        std::thread::sleep(Duration::from_secs(2));
+    }
+}
+
+fn clip_thread(lock: Arc<Mutex<()>>) {
+    loop {
+        let guard = lock.lock().unwrap();
+        println!("clip_thread");
+        drop(guard);
+        std::thread::sleep(Duration::from_secs(2));
+    }
+}
+
+fn launch_daemon() {
+    println!("Starting daemon...");
+
+    // Create a shared mutex lock
+    let lock = Arc::new(Mutex::new(()));
+
+    // Spawn two threads that share the mutex lock
+    let fifo_lock_clone = Arc::clone(&lock);
+    thread::spawn(move || fifo_thread(fifo_lock_clone));
+    let clip_lock_clone = Arc::clone(&lock);
+    thread::spawn(move || clip_thread(clip_lock_clone));
+
+    // Keep the daemon running indefinitely
+    loop {
+        thread::sleep(Duration::from_secs(1));
     }
 }
 
@@ -40,7 +80,10 @@ fn main() {
             client_speak_fifo("delete", Some(id));
         },
         "save" => client_speak_fifo("save", None),
-        "daemon" => println!("Daemon selected"),
+        "daemon" => {
+            launch_daemon();
+            process::exit(0);
+        },
         "help" => println!("Help selected"),
         _ => println!("Invalid argument"),
     }
